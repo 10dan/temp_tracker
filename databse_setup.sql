79f8f831521c0c69c797469a44ec0b15f275caa6
10dan <dan.anderson174@gmail.com>
2023-08-02 12:37:35 +0100

    we like lowercase

--- databse_setup.sql
+++ databse_setup.sql
@@ -6,9 +6,9 @@ create database temperature_tracking;
 grant all privileges on database temperature_tracking to dbadmin;
 
 -- psql -U dbadmin -d temperature_tracking
-CREATE TABLE temperatures (
-    time_stamp TIMESTAMPTZ NOT NULL DEFAULT NOW(),
-    temperature DECIMAL(6,3) NOT NULL
+create table temperatures (
+    time_stamp timestamptz not null default now(),
+    temperature decimal(6,3) not null
 );
 
 insert into temperatures (temperature) values (25.3);
