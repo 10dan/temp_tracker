45ce5345a0e1130f4896d6ed96f3ee6a0f306ede
10dan <dan.anderson174@gmail.com>
2023-08-04 16:26:25 +0100

    added noises

--- idle_script.py
+++ idle_script.py
@@ -4,42 +4,50 @@ import time
 from monitor import TemperatureMonitor
 from db_connect import DatabaseConnection
 import webserver
+from playsound import playsound
 
 lock = threading.Lock()
+
 button_pin = 23
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(button_pin, GPIO.IN)
 
+def play_end_noise():
+    for _ in range(5):
+        playsound('end.mp3')
+        time.sleep(0.2)
 
 def button_callback(channel):
+
     if lock.acquire(False):
         try:
             if (
                 GPIO.input(button_pin) == GPIO.HIGH
             ):  # Check if button is currently being pressed
-                print(f"Button pressed, GPIO pin {channel} signal on.")
-                result = take_measurment()
-                print(result)
+                playsound('start.mp3')
+                take_measurment()
+                play_end_noise()
+                
         finally:
             lock.release()
 
 
 GPIO.add_event_detect(button_pin, GPIO.RISING, callback=button_callback, bouncetime=200)
 
-
 def take_measurment():
     monitor = TemperatureMonitor()
-    db_connection = DatabaseConnection()
-    start = time.time()
     while True:
         temp = monitor.read_temp()
         monitor.add_temp(temp)
-        print(temp)
         if monitor.check_stable():
+            db_connection = DatabaseConnection()
             db_connection.log_temp(temp)
             db_connection.close()
-            return f"Measurement recorded in {time.time() - start:.2f}"
-
+            break
         time.sleep(1)
 
-webserver.start_webserver()
+
+try:
+    webserver.start_webserver()
+except KeyboardInterrupt:
+    GPIO.cleanup([button_pin])
--- webserver.py
+++ webserver.py
@@ -24,4 +24,4 @@ def home():
 
 
 def start_webserver():
-    app.run(debug=True, host="0.0.0.0", use_reloader=False)
+    app.run(debug=True, host="0.0.0.0",port=5001, use_reloader=False)
