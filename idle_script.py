db80941fd9c28d4b21796bf6a792108e27626293
10dan <dan.anderson174@gmail.com>
2023-08-04 12:32:08 +0100

    refactor sending into idle_script

--- idle_script.py
+++ idle_script.py
@@ -1,31 +1,51 @@
 import RPi.GPIO as GPIO
-import time
-import sensing
 import threading
-
-GPIO.setmode(GPIO.BCM)
-
-button_pin = 23
-GPIO.setup(button_pin, GPIO.IN)
+import time
+from monitor import TemperatureMonitor
+from db_connect import DatabaseConnection
 
 lock = threading.Lock()
+button_pin = 23
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(button_pin, GPIO.IN)
+
 
 def button_callback(channel):
     if lock.acquire(False):
         try:
-            if GPIO.input(button_pin) == GPIO.HIGH:  # Check if button is currently being pressed
+            if (
+                GPIO.input(button_pin) == GPIO.HIGH
+            ):  # Check if button is currently being pressed
                 print(f"Button pressed, GPIO pin {channel} signal on.")
-                result = sensing.take_measurment()
+                result = take_measurment()
                 print(result)
         finally:
-            lock.release() 
+            lock.release()
+
 
 GPIO.add_event_detect(button_pin, GPIO.RISING, callback=button_callback, bouncetime=200)
+
+
+def take_measurment():
+    monitor = TemperatureMonitor()
+    db_connection = DatabaseConnection()
+    start = time.time()
+    while True:
+        temp = monitor.read_temp()
+        monitor.add_temp(temp)
+        print(temp)
+        if monitor.check_stable():
+            db_connection.log_temp(temp)
+            db_connection.close()
+            return f"Measurement recorded in {time.time() - start:.2f}"
+
+        time.sleep(1)
+
 
 try:
     while True:
         time.sleep(0.1)
         pass
 except KeyboardInterrupt:
-    print("ancelled.") # ^Cancelled 
+    print("ancelled.")  # ^Cancelled
     GPIO.cleanup()
